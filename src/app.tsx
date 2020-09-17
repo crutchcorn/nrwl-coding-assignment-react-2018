86f553cd58d127edb91bc1caa679215971a43770
Corbin Crutchley <crutchcorn@gmail.com>
2020-09-16 19:13:24 -0700

    Initial work to provide centralize store using reducer and context

--- src/app.tsx
+++ src/app.tsx
@@ -1,42 +1,32 @@
 import * as React from "react";
-import {BackendService, Ticket} from "./backend";
+import {BackendService} from "./backend";
 import {take} from "rxjs/operators";
+import {TicketDataProvider, useTicketData} from "./context";
 
 interface AppProps {
     backend: BackendService;
 }
 
-const App = ({backend}: AppProps) => {
-    const [tickets, setTickets] = React.useState<Ticket[] | null>(null);
-
-    React.useEffect(() => {
-        backend.tickets()
-            .pipe(take(1))
-            .subscribe(ts => {
-                /**
-                 * If I don't do the "spread" here, it will cause the array to mutate. This is a limiation of not having
-                 * a server-backend. However, I'm determined to treat the "backend" as if it were a server one, so I won't be
-                 * fixing this upstream
-                 */
-                setTickets([...ts]);
-            });
-    }, []);
+const AppBase = ({backend}: AppProps) => {
+    const {state, dispatch} = useTicketData();
 
     const addTicket = () => {
+        dispatch({type: 'loadingAction'});
         backend.newTicket({description: "This is a test"})
             .pipe(take(1))
             .toPromise()
             .then(newTick => {
-                setTickets(tix => [...(tix || []), newTick])
+                dispatch({type: 'addTicket', payload: newTick});
             });
     }
 
     return (
         <div>
             <h2>Tickets</h2>
-            {tickets ? (
+            {state.loading && <p aria-live='polite'>Loading...</p>}
+            {state.tickets ? (
                 <ul>
-                    {tickets.map(t => (
+                    {state.tickets.map(t => (
                         <li key={t.id}>
                             Ticket: {t.id}, {t.description}
                         </li>
@@ -49,5 +39,11 @@ const App = ({backend}: AppProps) => {
         </div>
     );
 };
+
+const App = ({backend}: AppProps) => {
+    return <TicketDataProvider backend={backend}>
+        <AppBase backend={backend}/>
+    </TicketDataProvider>
+}
 
 export default App;
---
+++ src/context.tsx
@@ -0,0 +1,66 @@
+import * as React from "react";
+import {BackendService, Ticket} from "./backend";
+import {take} from "rxjs/operators";
+import {useReducer} from "react";
+
+interface ContextProviderProps {
+    backend: BackendService;
+}
+
+const initialState = {loading: true as boolean, tickets: [] as Ticket[]};
+
+type ReducerState = typeof initialState;
+
+function reducer(state: ReducerState, action: { type: string, payload?: any }): ReducerState {
+    switch (action.type) {
+        case 'initialDataLoad':
+            return {loading: false, tickets: action.payload};
+        case 'addTicket':
+            return {loading: false, tickets: [...state.tickets, action.payload]};
+        case 'loadingAction':
+            return {...state, loading: true};
+        default:
+            throw new Error();
+    }
+}
+
+export const useTickets = ({backend}: ContextProviderProps) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    React.useEffect(() => {
+        backend.tickets()
+            .pipe(take(1))
+            .subscribe(ts => {
+                /**
+                 * If I don't do the "spread" here, it will cause the array to mutate. This is a limiation of not having
+                 * a server-backend. However, I'm determined to treat the "backend" as if it were a server one, so I won't be
+                 * fixing this upstream
+                 */
+                dispatch({type: 'initialDataLoad', payload: [...ts]});
+            });
+    }, []);
+
+    return {
+        state,
+        dispatch
+    }
+}
+
+type TicketContextType = ReturnType<typeof useTickets>;
+
+export const TicketContext = React.createContext<TicketContextType>({
+    dispatch: () => {
+    },
+    state: initialState,
+});
+
+export const TicketDataProvider: React.FC<ContextProviderProps> = ({children, backend}) => {
+    const value = useTickets({backend})
+    return <TicketContext.Provider value={value}>
+        {children}
+    </TicketContext.Provider>
+}
+
+export const useTicketData = () => {
+    return React.useContext(TicketContext);
+}
