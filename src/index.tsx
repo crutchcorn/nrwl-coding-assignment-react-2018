5038cfa504eb33de2077ab810407c54a46c8772a
Corbin Crutchley <crutchcorn@gmail.com>
2020-09-18 12:02:29 -0700

    Fix bug caused by NTFS casing

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BackendService } from './backend';
-import App from './App';
+import App from './app';
 import * as serviceWorker from './serviceWorker';
 
 import "./index.css";
